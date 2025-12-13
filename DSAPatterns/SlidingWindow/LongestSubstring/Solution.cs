233513af4c5cf39369688083492eb7cb2fee16fc
VMahluza <mahluzavictor@gmail.com>
2025-12-12 23:04:52 -0800

    Implement sliding window for longest unique substring
    Replaced NotImplementedException with a sliding window algorithm in LengthOfLongestSubString. Utilizes two pointers and a dictionary to efficiently find the length of the longest substring without repeating characters.

--- DSAPatterns/SlidingWindow/LongestSubstring/Solution.cs
+++ DSAPatterns/SlidingWindow/LongestSubstring/Solution.cs
@@ -8,6 +8,23 @@ public class Solution
 {
     public int LengthOfLongestSubString(string s)
     { 
-        throw new NotImplementedException();
+        int LeftPointer = 0;
+        int LongestSoFar = 0;
+        Dictionary<char, int> LastVisitedOnPointer = new Dictionary<char, int>();
+
+        for (int RightPointer = 0; RightPointer < s.Length; RightPointer++)
+        { 
+        
+            char currentCharInS = s[RightPointer];
+            
+            LeftPointer = LastVisitedOnPointer.ContainsKey(currentCharInS) ? 
+                Math.Max(LeftPointer, LastVisitedOnPointer[currentCharInS] + 1) : LeftPointer;
+            
+            LastVisitedOnPointer[currentCharInS] = RightPointer;
+            LongestSoFar = Math.Max(LongestSoFar, RightPointer - LeftPointer + 1);
+
+        }
+
+        return LongestSoFar;
     }
 }
